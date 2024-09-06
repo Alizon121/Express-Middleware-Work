ae0d31d3adcf51ba5c2a22fba1b89d3aa4d8cd10
Andrew <andrew.lizon12@gmail.com>
2024-09-06 15:19:06 -0700

    Working on adding a middleware function

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+const errorHandler = require('express-async-errors')
+
+
+// Middleware function for logging method and URL path of the request
+
+
+
+// For testing purposes, GET /
+app.get('/', (req, res) => {
+  res.json("Express server running. No content provided at root level. Please use another route.");
+});
+
+// For testing express.json middleware
+app.post('/test-json', (req, res, next) => {
+  // send the body as JSON with a Content-Type header of "application/json"
+  // finishes the response, res.end()
+  res.json(req.body);
+  next();
+});
+
+// For testing express-async-errors
+app.get('/test-error', async (req, res) => {
+  throw new Error("Hello World!")
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
